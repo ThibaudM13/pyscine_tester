1533a3ad28e92e20d9f70112ab7658d9d71c5ebe
Thibo_M <thibaud.marin@proton.me>
2024-02-13 01:48:42 +0100

    Update launcher
    - Edit logo tester
    - Added checks on requirements
    - Added checks on virtual env success

--- test_launcher.sh
+++ test_launcher.sh
@@ -17,10 +17,38 @@ days[01]=Array
 
 days_available="${!days[@]}"
 
+function error() {
+	printf "\033[91;1m$1\033[m\n"
+	OPT_CACHE=0
+	delete_files
+}
+
+function check_dependencies() {
+	python3 --version 2>/dev/null 1>/dev/null
+	if (($? != 0)); then
+		error "Python3 is not installed."
+	fi
+
+	python3 -c "import venv" 2>/dev/null 1>/dev/null
+	if (($? != 0)); then
+		error "Virtual env python package (venv) not installed."
+	fi
+}
+
 chosen_day=0
 function get_user_choice() {
 	printf "\033[H\033[2J"
+<<<<<<< HEAD
 	printf "\033[1;96m ###   python3 Piscine Tester   ###\n\n\033[m"
+=======
+	printf "\033[1;92m
+  ┌──────────────────────────────────────────────────────────────┐
+  │  ╔═╗┬ ┬┌┬┐┬ ┬┌─┐┌┐┌  ╔═╗┬┌─┐┌─┐┬ ┌┐┌┌─┐  ╔╦╗┌─┐┌─┐┌┬┐┌─┐┬─┐  │
+  │  ╠═╝└┬┘ │ ├─┤│ ││││  ╠═╝│└─┐│  │ │││├┤    ║ ├┤ └─┐ │ ├┤ ├┬┘  │
+  │  ╩   ┴  ┴ ┴ ┴└─┘┘└┘  ╩  ┴└─┘└─┘┴ ┘└┘└─┘   ╩ └─┘└─┘ ┴ └─┘┴└─  │
+  └──────────────────────────────────────────────────────────────┘
+\n\033[m"
+>>>>>>> 63eabab (Update launcher)
 
 	printf "\033[1m Days available are:\033[m\n"
 	for key in $days_available; do
@@ -138,28 +166,38 @@ function delete_files() {
 	fi
 }
 
+function create_env() {
+	if ! [ -d ./PYscine_tester/.venv_tester ]; then
+		python3 -m venv ./PYscine_tester/.venv_tester &
+		loading "$!" "Creating virtual environnement for tester"
+	else
+		sleep 0.1
+		loading "$!" "Virtual Env already present"
+	fi
+
+	! [ -d ./PYscine_tester/.venv_tester ] && error "Tester virtual env non created." && kill -INT $$
+
+	source ./PYscine_tester/.venv_tester/bin/activate
+
+	[ -f ./requirement.txt ] && pip install -r ./requirements.txt 2>&1 >/dev/null  &
+	pip install -r ./PYscine_tester/tester_requirements.txt  2>&1 >/dev/null &
+	loading "$!" "Downloading dependencies"
+}
+
+check_dependencies
 get_user_choice
 
 trap delete_files INT
 
 load_files
 
-if ! [ -d ./PYscine_tester/.venv_tester ]; then
-	python3 -m venv ./PYscine_tester/.venv_tester &
-	loading "$!" "Creating virtual environnement for tester"
-else
-	sleep 0.1
-	loading "$!" "Virtual Env already present"
-fi
-
-! [ -d ./PYscine_tester/.venv_tester ] && echo "Virtual env inexistant" && kill -INT $$
-
-source ./PYscine_tester/.venv_tester/bin/activate
-
-[ -f ./requirement.txt ] && pip install -r ./requirements.txt 2>&1 >/dev/null  &
-pip install -r ./PYscine_tester/tester_requirements.txt  2>&1 >/dev/null &
-loading "$!" "Downloading dependencies"
+create_env
 
 pytest -v -rP "./PYscine_tester/ft_tester_day$chosen_day.py"
 
 delete_files
+# Clear the terminal
+clear -x
+
+# Move the cursor to the top left corner
+echo -e "\033[H"
