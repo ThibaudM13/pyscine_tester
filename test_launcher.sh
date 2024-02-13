cd5bf8e3f0789f1b86790ad9e215caae2a4089d0
Thibo_M <thibaud.marin@proton.me>
2024-02-13 02:56:19 +0100

    Update test_launcher

--- test_launcher.sh
+++ test_launcher.sh
@@ -2,41 +2,59 @@
 
 OPT_CACHE=0
 OPT_FORCE=0
-for arg in "$@"; do
-	if [ "$arg" == "-c" ] || [ "$arg" == "--allow-cache" ]; then
-		OPT_CACHE=1
-	elif [ "$arg" == "-f" ] || [ "$arg" == "--force" ]; then
-		OPT_FORCE=1
-	fi
-done
-
 declare -A days
+days_available=''
 
-days[00]=Starting
-days[01]=Array
+main() {
+	for arg in "$@"; do
+		case "$arg" in
+			-c|--allow-cache)
+				OPT_CACHE=1
+				continue;;
+			-f|--force)
+				OPT_FORCE=1
+				continue;;
+			*)
+				error "$arg: Unknown param..."
+				exit 1;;
+		esac
+	done
 
-days_available="${!days[@]}"
+	check_dependencies
 
-function error() {
+	days[00]=Starting
+	days[01]=Array
+
+	days_available="${!days[@]}"
+
+	trap delete_files INT
+	get_user_choice
+	load_files
+	create_env
+
+	pytest -v -rP "./PYscine_tester/ft_tester_day$chosen_day.py"
+
+	delete_files
+}
+
+# Print error message and disable caching.
+error() {
 	printf "\033[91;1m$1\033[m\n"
 	OPT_CACHE=0
 	delete_files
 }
 
-function check_dependencies() {
-	python3 --version 2>/dev/null 1>/dev/null
-	if (($? != 0)); then
-		error "Python3 is not installed."
-	fi
-
-	python3 -c "import venv" 2>/dev/null 1>/dev/null
-	if (($? != 0)); then
-		error "Virtual env python package (venv) not installed."
-	fi
+# Check dependencies: Python3 and venv.
+check_dependencies() {
+    python3 --version &>/dev/null || error "Python3 is not installed."
+    python3 -c "import venv" &>/dev/null || error "python package venv is not installed."
 }
 
+# Initialize chosen day variable
 chosen_day=0
-function get_user_choice() {
+
+# Function to prompt user for a day selection
+get_user_choice() {
 	printf "\033[H\033[2J"
 	printf "\033[1;92m
   ┌──────────────────────────────────────────────────────────────┐
@@ -46,6 +64,7 @@ function get_user_choice() {
   └──────────────────────────────────────────────────────────────┘
 \n\033[m"
 
+    # List available days
 	printf "\033[1m Days available are:\033[m\n"
 	for key in $days_available; do
 		printf "\t day$key \033[2m${days[$key]}\033[m\n"
@@ -53,25 +72,25 @@ function get_user_choice() {
 
 	printf "\n"
 
-	while true; do
-		printf '\r\033[2K'
-		read -n 1 -p 'Enter the wished day [0-1] ' choice 
-		if ! [[ -n $choice ]]; then
-			continue
-		elif ! [[ $choice =~ [0-9] ]]; then
-			continue
-		elif (( choice <= 1 && choice >= 0 )); then
+    # Prompt user for day selection
+    while true; do
+        printf '\r\033[2K'
+        read -n  1 -p 'Enter the wished day [0-1] ' choice
+        if ! [[ -n $choice ]] || ! [[ $choice =~ [0-9] ]] || ! (( choice <=  1 && choice >=  0 )); then
+            sleep  0.3
+            continue
+		else
 			break
 		fi
-		sleep 0.3
-	done
+    done
 
 	printf "\n"
 
+    # Store user's choice
 	chosen_day=$choice
 }
 
-function loading() {
+loading() {
     loading_pid=$1
     loading_str=$2
     loading_chars=("⠾" "⠽" "⠻" "⠟" "⠯" "⠷")
@@ -102,10 +121,9 @@ function loading() {
 
     # Clear the line after the loop ends
     printf "\r\033[K\e[?25h \033[92;1m✓\033[m $loading_str\n"
-
 } 
 
-function load_files() {
+load_files() {
 	base_url="https://gitlab.com/ThibaudM13/piscine_py_tester/-/raw/main"
 	req="$base_url/day0$chosen_day.req"
 	cfg="$base_url/day0$chosen_day.cfg"
@@ -118,59 +136,58 @@ function load_files() {
 		ln -sf ./PYscine_tester/tester_launcher.sh tester_launcher.sh
 	fi
 
-	if ! [ -f ./PYscine_tester/conftest.py ] || ((OPT_FORCE==1)); then
-		wget "$cfg" -O ./PYscine_tester/conftest.py -o /dev/null &
-		loading "$!" "Downloading config file"
-	else
-		sleep 0.05
-		loading "$!" "Config file already here (use -f to force)"
-	fi
-	ln -sf ./PYscine_tester/conftest.py ./conftest.py
 
-	if ! [ -f ./PYscine_tester/tester_requirements.txt ] || ((OPT_FORCE==1)); then
-		wget "$req" -O ./PYscine_tester/tester_requirements.txt -o /dev/null &
-		loading "$!" "Downloading requirement file"
-	else
-		sleep 0.05
-		loading "$!" "Requirement file already here (use -f to force)"
-	fi
+    download_or_skip() {
+        local url=$1
+        local dest=$2
+        local msg=$3
+        if ! [ -f $dest ] || ((OPT_FORCE==1)); then
+            wget "$url" -O $dest -o /dev/null &
+            loading "$!" "$msg"
+        else
+            sleep  0.05
+            loading "$!" "$(basename $dest) already here (use -f to force)"
+        fi
+    }
 
-	if ! [ -f "./PYscine_tester/ft_tester_day$chosen_day.py" ] || ((OPT_FORCE==1)); then
-		wget "$tests" -O "./PYscine_tester/ft_tester_day$chosen_day.py" -o /dev/null &
-		loading "$!" "Downloading test script"
-	else
-		sleep 0.05
-		loading "$!" "Test script already here (use -f to force)"
-	fi
+    download_or_skip "$cfg" "./PYscine_tester/conftest.py" "Downloading config file"
+    ln -sf ./PYscine_tester/conftest.py ./conftest.py
+    download_or_skip "$req" "./PYscine_tester/tester_requirements.txt" "Downloading requirement file"
+    download_or_skip "$tests" "./PYscine_tester/ft_tester_day$chosen_day.py" "Downloading test script"
 }
 
-function delete_files() {
+delete_files() {
 	printf "\n"
+
+    # Stop any background jobs related to the tester
 	kill $(jobs -p)  2>/dev/null &
 	loading "$!" "Stopping tester"
 
 	rm -f ./conftest.py
+
+    # Check if OPT_CACHE is set to  0, indicating no caching should occur
 	if ((OPT_CACHE == 0)); then
 		deactivate 2>/dev/null
 
-		rm -rf ./PYscine_tester 2>/dev/null &
+		# Asynchronously remove the PYscine_tester directory
+		rm -rf ./PYscine_tester &>/dev/null &
 		loading "$!" "Clearing directory."
 
-		exec rm -rf ./PYscine_tester ./tester_launcher.sh
+		# Synchronously remove the PYscine_tester directory and tester_launcher.sh
+		exec rm -rf ./PYscine_tester
 	else
+        # If caching is allowed, simply exit without cleanup
 		exit 0
 	fi
 }
 
-function create_env() {
+create_env() {
 	if ! [ -d ./PYscine_tester/.venv_tester ]; then
 		python3 -m venv ./PYscine_tester/.venv_tester &
 		loading "$!" "Creating virtual environnement for tester"
-	else
-		sleep 0.1
-		loading "$!" "Virtual Env already present"
 	fi
 
+    # Verify that the virtual environment was created successfully
 	! [ -d ./PYscine_tester/.venv_tester ] && error "Tester virtual env non created." && kill -INT $$
 
 	source ./PYscine_tester/.venv_tester/bin/activate
@@ -180,20 +197,4 @@ function create_env() {
 	loading "$!" "Downloading dependencies"
 }
 
-check_dependencies
-get_user_choice
-
-trap delete_files INT
-
-load_files
-
-create_env
-
-pytest -v -rP "./PYscine_tester/ft_tester_day$chosen_day.py"
-
-delete_files
-# Clear the terminal
-clear -x
-
-# Move the cursor to the top left corner
-echo -e "\033[H"
+main "$@"; exit
